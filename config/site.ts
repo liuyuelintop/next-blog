24a27d7bd509897fd79f56a06618f5b2786f714d
liuyuelintop <liuyuelintop@gmail.com>
2024-05-15 17:48:25 +1000

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "YuelinBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Yuelin Liu",
+  links: {
+    instagram: "https://www.instagram.com",
+    github: "https://github.com/liuyuelintop",
+    linkedin: "https://www.linkedin.com/in/yuelin-liu-867ab6259/",
+    portfolio: "https://react-portfolio-tau-eight-56.vercel.app/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
