ce453068edbcef4f07cd4c6bd3f28789dfdc6baa
liuyuelintop <liuyuelintop@gmail.com>
2024-05-25 14:43:22 +1000

    add react junior developer interview questions; add safe type in Callout component

--- components/callout.tsx
+++ components/callout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 interface CalloutProps {
   children?: ReactNode;
-  type?: "default" | "warning" | "danger";
+  type?: "default" | "safe" | "warning" | "danger";
 }
 
 export function Callout({
@@ -16,6 +16,7 @@ export function Callout({
       className={cn(
         "my-6 items-start rounded-md border boder-l-4 p-4 w-full dark:max-w-none",
         {
+          "border-green-900 bg-green-50 dark:prose": type === "safe",
           "border-red-900 bg-red-50 dark:prose": type === "danger",
           "border-yellow-900 bg-yellow-50 dark:prose": type === "warning",
         }
