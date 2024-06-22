d9d4a20eeddbfe33f5abedf3c147a2faabe6115f
liuyuelintop <liuyuelintop@gmail.com>
2024-06-22 12:04:36 +1000

    FIx slug page UI: tag overflow

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -73,14 +73,14 @@ export default async function PostPage({ params }: PostPageProps) {
 
   return (
     <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-      <h1 className="mb-2">{post.title}</h1>
-      <div className="flex gap-2 mb-2">
+      <h2 className="mb-4 text-2xl md:text-4xl">{post.title}</h2>
+      <div className="flex gap-2 mb-2 flex-wrap">
         {post.tags?.map((tag) => (
           <Tag tag={tag} key={tag} />
         ))}
       </div>
       {post.description ? (
-        <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
+        <p className="text-lg mt-0 text-muted-foreground">{post.description}</p>
       ) : null}
       <hr className="my-4" />
       <MDXContent code={post.body} />
